f7c3e1daf92925baf8bcdfc769604ea1f0739aef
Vicente Obregon <desarrollogis@gmail.com>
2019-05-01 12:57:43 -0700

    elevator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CubeComponent } from './cube/cube.component';
+import { ElevatorComponent } from './elevator/elevator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CubeComponent,
+    ElevatorComponent
   ],
   imports: [
     BrowserModule
