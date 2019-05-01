8f48999279ab73b16adcf7efebad0285514aa6b5
Vicente Obregon <desarrollogis@gmail.com>
2019-05-01 16:25:54 -0700

    events

--- src/app/elevator/elevator.component.ts
+++ src/app/elevator/elevator.component.ts
@@ -8,16 +8,22 @@ import { ControllerService } from '../controller.service';
   styleUrls: ['./elevator.component.scss']
 })
 export class ElevatorComponent implements OnInit {
+  events = false;
+
   constructor(public controllerService: ControllerService) { }
 
   ngOnInit() {
   }
 
   ngAfterViewChecked() {
+    if (this.events) {
+      return;
+    }
+
     const elements = document.getElementsByClassName('elevator');
 
-    console.log(elements.length);
     for (let i = 0, m = elements.length; i < m; ++i) {
+      this.events = true;
       elements[i].addEventListener('transitionend', () => {
         this.controllerService.currentFloor = this.controllerService.transitionFloor;
         this.controllerService.move();
