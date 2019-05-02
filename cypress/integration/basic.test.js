fdb3c7245f14dffe8dc62d0ff4c0486b28422517
Vicente Obregon <desarrollogis@gmail.com>
2019-05-02 07:57:22 -0700

    testing

--- cypress/integration/basic.test.js
+++ cypress/integration/basic.test.js
@@ -1,15 +1,18 @@
 describe('Elevator', () => {
-  context.only('Internal buttons', () => {
+  context('Internal buttons', () => {
     it('should work', () => {
       cy.visit('http://localhost:4200/');
       cy.get('div.elevator').should('have.class', 'floor-0');
       cy.get('div.door').should('have.class', 'closed');
-      cy.get('button.floor-0.floor-up').click();
+      cy.get('button.external.floor-0.floor-up').click();
       cy.get('div.door').should('have.class', 'opening');
       cy.get('div.door').should('have.class', 'open');
       cy.get('div.door').should('have.class', 'closing');
       cy.get('div.door').should('have.class', 'closed');
+      cy.get('div.elevator').should('have.class', 'floor-0');
       cy.get('button.internal.floor-3').click();
+      cy.get('div.elevator').should('have.class', 'floor-1');
+      cy.get('div.elevator').should('have.class', 'floor-2');
       cy.get('div.elevator').should('have.class', 'floor-3');
       cy.get('div.door').should('have.class', 'opening');
       cy.get('div.door').should('have.class', 'open');
@@ -21,11 +24,15 @@ describe('Elevator', () => {
     it('should work', () => {
       cy.visit('http://localhost:4200/');
       cy.get('div.elevator').should('have.class', 'floor-0');
-      cy.get('div.door').should('have.class', 'close');
-      cy.get('button.floor-3.floor-down').click();
+      cy.get('div.door').should('have.class', 'closed');
+      cy.get('button.external.floor-3.floor-down').click();
+      cy.get('div.elevator').should('have.class', 'floor-1');
+      cy.get('div.elevator').should('have.class', 'floor-2');
       cy.get('div.elevator').should('have.class', 'floor-3');
+      cy.get('div.door').should('have.class', 'opening');
       cy.get('div.door').should('have.class', 'open');
-      cy.get('div.door').should('have.class', 'close');
+      cy.get('div.door').should('have.class', 'closing');
+      cy.get('div.door').should('have.class', 'closed');
     });
   });
 });
